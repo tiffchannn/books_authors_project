b3087eceb85ba8ce5e2b10d35032bf6abfae7e09
tiffchannn <tiffany.yi.chan@gmail.com>
2020-12-12 09:07:25 -0800

    Refactor method names and paths

--- books_authors_app/urls.py
+++ books_authors_app/urls.py
@@ -5,8 +5,8 @@ urlpatterns = [
     path('', views.index),
     path('addBook', views.addBook),
     path('books/<int:book_id>', views.displayBook),
-    path('processAddingAuthor', views.addAuthorToBook),
-    path('processAddingBook', views.addBookToAuthor),
+    path('addAuthorToBook', views.addAuthorToBook),
+    path('addBookToAuthor', views.addBookToAuthor),
     path('authors/<int:author_id>', views.displayAuthor),
     path('authors', views.author),
     path('addAuthor', views.addAuthor)
